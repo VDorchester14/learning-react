50df1fb75ab3a57a15f7c49808b0321ce14ce7a4
VDorchester14 <vail.dorchester@colorad.edu>
2017-12-24 14:14:34 -0700

    removable components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import HelloWorld from './HelloWorld.js';
+import HelloWorldList from './HelloWorldList.js';
 
 const App = () => {
   return (
     <div className="App">
-      <HelloWorld name="Vail"/>
-      <HelloWorld name="Leland sucks butt hole"/>
+      <HelloWorldList />
     </div>
   );
 };
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './HelloWorld.css';
 
-const HelloWorld = props => {
-  return (<div className="HelloWorld">Hello {props.name}!</div>);
-};
+class HelloWorld extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { greeting: 'Hello' };
+        this.frenchify = this.frenchify.bind(this);
+        this.englishify = this.englishify.bind(this);
+        this.removeGreeting = this.removeGreeting.bind(this);
+    }
+
+    englishify() {
+        this.setState({ greeting: "Hello" });
+    }
+
+    frenchify() {
+        this.setState({ greeting: 'Bonjour' });
+    }
+
+    removeGreeting() {
+        this.props.removeGreeting(this.props.name);
+    }
+
+    render() {
+        return (
+            <div className="HelloWorld">
+                {this.state.greeting} {this.props.name}!
+                <br/>
+                <button className="frenchify" onClick={this.frenchify}>Frenchify!</button>
+                <button className="englishify" onClick={this.englishify}>Englishify!</button>
+                <br/>
+                <button className="removeMe" onClick={this.removeGreeting}>Remove Me!</button>
+            </div>
+        );
+    }
+
+}
 
 export default HelloWorld;
